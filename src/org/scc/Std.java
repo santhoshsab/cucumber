db4e21d82de766a053691d48a9d9d902cd433795
santhoshsab <sabarisanthosh023@gmail.com>
2022-08-30 15:07:06 +0530

    new code added

--- src/org/scc/Std.java
+++ src/org/scc/Std.java
@@ -4,6 +4,9 @@ public class Std {
 	
 	public void stdAddress() {
 		System.out.println("student address is omr");
+		System.out.println("student address is ecr");
+		System.out.println("student address is vada");
+		
 
 	}
 	public void stdName() {
